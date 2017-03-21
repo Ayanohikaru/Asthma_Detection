f2faa398c47b80a3815206a05b34358f4ba92ac7
Ayanohikaru <ayanohikaru@gmail.com>
2017-03-22 03:55:20 +0700

    First step : change folder

--- Config.m
+++ Config.m
@@ -1,9 +1,14 @@
 %This code is used for location configuring between GitHub and local
 %respirotary. Change addresses in this file before running codes
 
-D_patientFolder = 'D:\MIMIC II WAVEFORM DATABASE\RAW MIMIC II DATABASE';
+% GitHub sync
 D_generateData = 'D:\Project\Asthma Detection\GENERATE DATA CODE'
-D_asthDATA = 'D:\Project\Asthma Detection\Asthma Data';
+D_AsthmaEXA = 'D:\Project\Asthma Detection\Asthma Exacerbation Extraction';
+
+% Create new folder
+D_asthDATA = 'D:\Project\Asthma Detection\Asthma Data'; %save 2min files
+
+% In database folder
+D_patientFolder = 'D:\MIMIC II WAVEFORM DATABASE\RAW MIMIC II DATABASE'; 
 D_info='D:\MIMIC II WAVEFORM DATABASE\RAW MIMIC II DATABASE\INFO';
 D_nRecords = 'D:\MIMIC II WAVEFORM DATABASE\RAW MIMIC II DATABASE\PRE_GENERATED RECORD';
-D_AsthmaEXA = 'D:\Project\Asthma Detection\Asthma Exacerbation Extraction';
